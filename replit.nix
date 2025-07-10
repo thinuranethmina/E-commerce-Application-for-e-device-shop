3e768cd7c34fc16207aca3c6399bf3ed0f979cb3
thinuranethmina <thinuranethmina@gmail.com>
2025-07-10 18:16:37 +0530

    Set up Laravel project for Replit deployment

---
+++ replit.nix
@@ -0,0 +1,15 @@
+{ pkgs }: {
+  deps = [
+    pkgs.php82
+    pkgs.php82Packages.composer
+    pkgs.php82Extensions.openssl
+    pkgs.php82Extensions.pdo
+    pkgs.php82Extensions.pdo_mysql
+    pkgs.php82Extensions.mbstring
+    pkgs.php82Extensions.tokenizer
+    pkgs.php82Extensions.xml
+    pkgs.php82Extensions.curl
+    pkgs.php82Extensions.zip
+    pkgs.mysql
+  ];
+}
